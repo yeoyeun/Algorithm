d3fe3de34a2513ee04584df06ead0774294a41f4
yeoyeun <yean1715@gmail.com>
2025-01-22 11:01:53 +0900

    [level 0] Title: 제곱수 판별하기, Time: 2.98 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i < n; i++){
+        if(n == i*i){
+            answer = 1;
+            break;
+        }else
+            answer = 2;
+    }
+    return answer;
+}
